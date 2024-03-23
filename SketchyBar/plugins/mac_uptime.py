79149d0011a15155d7ee5eb55cbe8622b465c5d7
laggedSkapari <es.rajatyadav@gmail.com>
2024-03-23 05:46:17 +0530

    fixed mac_uptime plugin

--- SketchyBar/plugins/mac_uptime.py
+++ SketchyBar/plugins/mac_uptime.py
@@ -5,12 +5,12 @@ import os
 def getUptime():
     os.system('uptime > ~/mac_uptime_details')
     uptimeDetails = open('/Users/laggedskapari/mac_uptime_details', 'r').read()
-    uptime = uptimeDetails.split('  ')[1].split(',')[0].split(" ")[1]
-    if ":" in uptime:
-        uptimeSplited = uptime.split(':')
-        return f"{uptimeSplited[0]} HRS {uptimeSplited[1]} MINS"
-    else:
-        return f"{uptime} MINS"
+    try:
+        uptime = uptimeDetails.split('  ')[1].split(',')[0].split(" ")[1]
+        return f"{uptime}"
+    except IndexError:
+        uptime = uptimeDetails.split()[2].split(":")
+        return f"{uptime[0]} HRS {uptime[1][:-1]} MINS"
 
 
 if __name__ == "__main__":
